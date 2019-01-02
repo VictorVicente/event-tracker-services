634cc6d881a1839fbf5cb70592730bcb71aa168d
Victor <victor.g.vicente@gmail.com>
2019-01-02 15:47:54 -0300

    Await fix

--- read-whitelist-service/index.js
+++ read-whitelist-service/index.js
@@ -30,69 +30,73 @@ if (typeof web3 !== 'undefined') {
     }
 }
 
-console.log(`Choosen network is ${selected_network.toUpperCase()}`);
-let scriptData = await dynamoDB.getItem('testFunction');
-console.log(scriptData);
+readData();
 
-let fromBlock = parseInt(scriptData.lastBlock) + 1;
-let toBlock = (await web3.eth.getBlock('latest')).number;
-console.log(`Last block No. used to get events: ${toBlock.toString()}`);
+async function readData() {
+    console.log(`Choosen network is ${selected_network.toUpperCase()}`);
+    let scriptData = await dynamoDB.getItem('testFunction');
+    console.log(scriptData);
 
-let eventSignature = web3.eth.abi.encodeEventSignature('LogModifyWhitelist(address,uint256,address,uint256,uint256,uint256,bool)');
+    let fromBlock = parseInt(scriptData.lastBlock) + 1;
+    let toBlock = (await web3.eth.getBlock('latest')).number;
+    console.log(`Last block No. used to get events: ${toBlock.toString()}`);
 
-let options = {
-    fromBlock: web3.utils.toHex(fromBlock),
-    toBlock: web3.utils.toHex(toBlock),
-    topics: [eventSignature]
-};
+    let eventSignature = web3.eth.abi.encodeEventSignature('LogModifyWhitelist(address,uint256,address,uint256,uint256,uint256,bool)');
 
-let logs = await web3.eth.getPastLogs(options);
-if (logs.length == 0) {
-    console.log("No logs were found.");
-} else {
-    let inputs = [
-        {
-            "indexed": false,
-            "name": "_investor",
-            "type": "address"
-        },
-        {
-            "indexed": false,
-            "name": "_dateAdded",
-            "type": "uint256"
-        },
-        {
-            "indexed": false,
-            "name": "_addedBy",
-            "type": "address"
-        },
-        {
-            "indexed": false,
-            "name": "_fromTime",
-            "type": "uint256"
-        },
-        {
-            "indexed": false,
-            "name": "_toTime",
-            "type": "uint256"
-        },
-        {
-            "indexed": false,
-            "name": "_expiryTime",
-            "type": "uint256"
-        },
-        {
-            "indexed": false,
-            "name": "_canBuyFromSTO",
-            "type": "bool"
-        }
-    ];
-    let events = getEventsFromLogs(inputs, logs, eventSignature);
-    await insertEventsIntoMySQL(events);
+    let options = {
+        fromBlock: web3.utils.toHex(fromBlock),
+        toBlock: web3.utils.toHex(toBlock),
+        topics: [eventSignature]
+    };
+
+    let logs = await web3.eth.getPastLogs(options);
+    if (logs.length == 0) {
+        console.log("No logs were found.");
+    } else {
+        let inputs = [
+            {
+                "indexed": false,
+                "name": "_investor",
+                "type": "address"
+            },
+            {
+                "indexed": false,
+                "name": "_dateAdded",
+                "type": "uint256"
+            },
+            {
+                "indexed": false,
+                "name": "_addedBy",
+                "type": "address"
+            },
+            {
+                "indexed": false,
+                "name": "_fromTime",
+                "type": "uint256"
+            },
+            {
+                "indexed": false,
+                "name": "_toTime",
+                "type": "uint256"
+            },
+            {
+                "indexed": false,
+                "name": "_expiryTime",
+                "type": "uint256"
+            },
+            {
+                "indexed": false,
+                "name": "_canBuyFromSTO",
+                "type": "bool"
+            }
+        ];
+        let events = getEventsFromLogs(inputs, logs, eventSignature);
+        await insertEventsIntoMySQL(events);
+    }
+
+    scriptData.lastBlock = toBlock;
+    await dynamoDB.putItem('testFunction', scriptData);
 }
-
-scriptData.lastBlock = toBlock;
-await dynamoDB.putItem('testFunction', scriptData);
 
 function getEventsFromLogs (inputs, logs, eventSignature) {
     let filteredLogs = logs.filter(l => l.topics.includes(eventSignature));
